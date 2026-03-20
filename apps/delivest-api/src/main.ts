525bb71e3fdacd443da97e0ad136e461f0938ddd
kiruxashafr <kirshafranov@gmail.com>
2026-03-20 22:14:31 +0300

    feat: prisma

--- apps/delivest-api/src/main.ts
+++ apps/delivest-api/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
