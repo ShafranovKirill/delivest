55cb7728efc93f4e2c8a64dc0b30d793cfc5eca9
kiruxashafr <kirshafranov@gmail.com>
2026-05-11 11:04:37 +0300

    feat: init

---
+++ apps/delivest-web/src/main.ts
@@ -0,0 +1,40 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import { MyPreset } from './theme'
+import router from './router'
+import './style.css'
+import 'primeicons/primeicons.css'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+import dayjs from 'dayjs'
+import 'dayjs/locale/ru'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import i18n from './i18n'
+import ToastService from 'primevue/toastservice'
+import { ConfirmationService } from 'primevue'
+
+dayjs.locale('ru')
+dayjs.extend(relativeTime)
+
+const app = createApp(App)
+app.use(ToastService)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(router)
+app.use(i18n)
+app.use(ConfirmationService)
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset,
+    options: {
+      darkModeSelector: '.p-dark',
+    },
+  },
+})
+app.use(pinia)
+
+app.mount('#app')
