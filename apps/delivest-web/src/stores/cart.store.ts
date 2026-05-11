a53bd580f90003c8fe2a514a60811e1d4643a25f
kiruxashafr <kirshafranov@gmail.com>
2026-05-11 23:14:23 +0300

    feat: cart store

---
+++ apps/delivest-web/src/stores/cart.store.ts
@@ -0,0 +1,89 @@
+import api from '@/api/axios'
+import type { AddToCartRequest, CartResponse, RemoveFromCartRequest } from '@delivest/types'
+import { defineStore } from 'pinia'
+import { useBranchStore } from './branch.store'
+
+export const useCartStore = defineStore('cart', {
+  state: () => ({
+    cart: null as CartResponse | null,
+    isLoading: false,
+  }),
+  getters: {
+    totalPrice: (state) => state.cart?.totalPrice ?? 0,
+    totalItems: (state) => state.cart?.totalItems ?? 0,
+  },
+  actions: {
+    async fetchCart() {
+      const branchStore = useBranchStore()
+      if (!branchStore.curentBranch) {
+        await branchStore.fetchBranches
+      }
+      this.isLoading = true
+      try {
+        const { data } = await api.get<CartResponse>(`/cart/${branchStore.curentBranch?.id}`)
+        this.cart = data
+      } catch (error) {
+        throw error
+      } finally {
+        this.isLoading = false
+      }
+    },
+
+    async addToCart(productId: string, quantity: number) {
+      if (!this.cart) {
+        await this.fetchCart
+      }
+      this.isLoading = true
+      try {
+        const payload: AddToCartRequest = {
+          cartId: this.cart!.id,
+          productId,
+          quantity,
+        }
+        const { data } = await api.post<CartResponse>('/cart/add', payload)
+        this.cart = data
+        return data
+      } catch (error) {
+        throw error
+      } finally {
+        this.isLoading = false
+      }
+    },
+    async removeFromCart(productId: string, deleteAll: boolean) {
+      if (!this.cart) {
+        await this.fetchCart
+      }
+      this.isLoading = true
+      try {
+        const payload: RemoveFromCartRequest = {
+          cartId: this.cart!.id,
+          productId,
+          deleteAll,
+        }
+        const { data } = await api.patch<CartResponse>('/cart/remove', payload)
+        this.cart = data
+        return data
+      } catch (error) {
+        throw error
+      } finally {
+        this.isLoading = false
+      }
+    },
+    async clearCart() {
+      if (!this.cart) {
+        return
+      }
+
+      this.isLoading = true
+
+      try {
+        await api.delete(`/cart/clear/${this.cart.id}`)
+        this.cart = null
+      } catch (error) {
+        throw error
+      } finally {
+        this.isLoading = false
+      }
+    },
+  },
+})
